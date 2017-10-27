33df43e71b5ce8072a0d3e7d4c128ac2d30fd4e3
FreemenL <liuxiajiang818@163.com>
2017-10-27 17:39:27 +0800

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,24 @@
 //开发环境配置
 const merge = require('webpack-merge');
+const webpack = require('webpack');
+const path = require('path');
 const common = require('./config/default.js');
-// const common = require('./webpack.common.js');
+
+common.plugins.push(new webpack.HotModuleReplacementPlugin());
 
 module.exports = merge(common,{
+	output:{
+		filename:'assets/[name].bundle.js',
+		path:path.resolve(__dirname,'../dist')
+	},
 	devtool:"inline-source-map",
 	devServer:{
 		contentBase:'./dist',
 		port:8000,
+		hot:true
 	}
 });
+
 
 
 
